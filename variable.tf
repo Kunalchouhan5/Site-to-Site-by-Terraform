a6ae26b0869ecc2522bc21c37846febff7e5a8cf
Kunalchouhan5 <chouhankunal984@gmail.com>
2025-10-15 15:36:08 +0530

    Changes in provider and variable file for the authentication of terraform

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "tenant_id" {
+  type = string
+  default = "6c96c136-8a5f-4c33-8ca1-48281b0aa8df"
+}
+
+variable "subscription" {
+  type = string
+  default = "ff796b21-0b11-4d2f-8d9a-23823e9d8709"
+}
