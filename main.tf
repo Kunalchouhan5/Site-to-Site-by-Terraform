b738297914ce01c607ff5bee4c2282eb92ff8c44
Kunalchouhan5 <chouhankunal984@gmail.com>
2025-10-15 17:50:20 +0530

    changes in variable and main file

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 # Resource Group - On-premises 
 resource "azurerm_resource_group" "on-premises-rg" {
-  name     = var.onprem-rg
+  name     = var.on_prem_rg
   location = var.location
 }
 
 # Virtual Network - On-prem
 resource "azurerm_virtual_network" "on-prem-vnet" {
-  name                = "test-poc-on-prem-vnet-01"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.on-premises-rg.location
+  name                = var.on-prem-vnet-name
+  address_space       = var.on-prem-address_space-1
+  location            = var.location
   resource_group_name = azurerm_resource_group.on-premises-rg.name
 }
 
 # Virtual Network Subnet
 resource "azurerm_subnet" "onprem-workload-subnet" {
-  name                 = "test-poc-onprem-wrkld-subnet-01"
+  name                 = var.on-prem-subnet-name
   resource_group_name  = azurerm_resource_group.on-premises-rg.name
   virtual_network_name = azurerm_virtual_network.on-prem-vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = var.on-prem-address_prefixes-1
 }
 
 # Public IP - 1
 resource "azurerm_public_ip" "on-prem-pip" {
-  name                = "test-poc-onprem-pip-01"
-  location            = azurerm_resource_group.on-premises-rg.location
+  name                = var.on-prem-public-ip 
+  location            = var.location
   resource_group_name = azurerm_resource_group.on-premises-rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
@@ -31,7 +31,7 @@ resource "azurerm_public_ip" "on-prem-pip" {
 
 # Network Security Group
 resource "azurerm_network_security_group" "onprem-nsg-1" {
-  name                = "test-poc-onprem-comp-nsg-01"
+  name                = var.on-prem-nsg
   location            = azurerm_resource_group.on-premises-rg.location
   resource_group_name = azurerm_resource_group.on-premises-rg.name
 }
@@ -73,7 +73,7 @@ resource "azurerm_network_interface" "onprem-nic-1" {
 
 # Windows Virtual Machine -- 1
 resource "azurerm_windows_virtual_machine" "Hyper-v" {
-  name                = "test-onp-vm"
+  name                = var.on-prem-hyper-v
   resource_group_name = azurerm_resource_group.on-premises-rg.name
   location            = azurerm_resource_group.on-premises-rg.location
   size                = "Standard_D4s_v3"
@@ -98,49 +98,49 @@ resource "azurerm_windows_virtual_machine" "Hyper-v" {
 
 
 # Resource Group - Hub-rg
-resource "azurerm_resource_group" "hub-net-rg" {
-  name     = "test-poc-hub-rg"
-  location = "East US"
+resource "azurerm_resource_group" "hub-resource_rg" {
+  name     = var.hub_rg
+  location = var.location
 }
 
 # Virtual Network - hub-Vnet
 resource "azurerm_virtual_network" "hub-vnet" {
-  name                = "test-poc-hub-vnet-01"
-  address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.sp-paas-rg.name
-  depends_on = [ azurerm_resource_group.sp-paas-rg, azurerm_resource_group.on-premises-rg ]
+  name                = var.hub-vnet-name
+  address_space       = var.hub_address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.hub-resource_rg.name
+  depends_on = [ azurerm_resource_group.hub-resource_rg, azurerm_resource_group.on-premises-rg ]
 }
 
 # Virtual Network Subnet
 resource "azurerm_subnet" "gateway" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.sp-paas-rg.name
+  name                 = var.hub-subnet-name-1
+  resource_group_name  = azurerm_resource_group.hub-resource_rg.name
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
-  address_prefixes     = ["10.1.0.0/26"]
+  address_prefixes     = var.hub_address_prefixes-1
 }
 
 # Virtual Network Subnet
 resource "azurerm_subnet" "hub-workload-subnet" {
-  name                 = "test-poc-hub-wrkld-subnet-01"
-  resource_group_name  = azurerm_resource_group.hub-net-rg.name
+  name                 = var.hub-subnet-name-2
+  resource_group_name  = azurerm_resource_group.hub-resource_rg.name
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
-  address_prefixes     = ["10.1.0.64/26"]
+  address_prefixes     = var.hub_address_prefixes-2
 }
 
 # Hub - VNG_PublicIP
 resource "azurerm_public_ip" "vng-pip" {
-  name                = "test-poc-vng-pip-01"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
+  name                = var.vng-public-ip
+  location            = var.location
+  resource_group_name = azurerm_resource_group.hub-resource_rg.name
   allocation_method   = "Static"
    sku                 = "Standard"
 }
 
 resource "azurerm_virtual_network_gateway" "VNG" {
-  name                = "test-poc-vng"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
+  name                = var.VNG-name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.hub-resource_rg.name
 
   type     = "Vpn"
   vpn_type = "RouteBased"
@@ -157,96 +157,19 @@ resource "azurerm_virtual_network_gateway" "VNG" {
   }
 }
 
-# Public IP - Hub Network
-resource "azurerm_public_ip" "hub-pip-01" {
-  name                = "test-poc-hub-pip-01"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Network Security Group - Hub Network
-resource "azurerm_network_security_group" "hub-nsg-1" {
-  name                = "test-poc-hub-comp-nsg-01"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
-}
-
-# Example NSG Rule (Allow SSH from any IP)
-resource "azurerm_network_security_rule" "hub-rule-01" {
-  name                        = "RDP-Allow"
-  priority                    = 310
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "3389"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.hub-net-rg.name
-  network_security_group_name = azurerm_network_security_group.hub-nsg-1.name
-}
-
-# Network security group associate 
-resource "azurerm_subnet_network_security_group_association" "hub-nsg-associate-1" {
-  subnet_id                 = azurerm_subnet.hub-workload-subnet.id
-  network_security_group_id = azurerm_network_security_group.hub-nsg-1.id
-}
-
-
-# Network Interface - Hub Network
-resource "azurerm_network_interface" "hub-nic-1" {
-  name                = "test-poc-hub-nic-1"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.hub-workload-subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.hub-pip-01.id
-  }
-}
-
-# Windows Virtual Machine  -- Hub Network
-resource "azurerm_windows_virtual_machine" "hub-vm" {
-  name                = "test-hub-vm"
-  resource_group_name = azurerm_resource_group.hub-comp-rg.name
-  location            = azurerm_resource_group.hub-comp-rg.location
-  size                = "Standard_D2s_v4"
-  admin_username      = "kunal"
-  admin_password      = "Admin@123456"
-  network_interface_ids = [
-    azurerm_network_interface.hub-nic-1.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
 # Local Network gateway - Hub Network
 resource "azurerm_local_network_gateway" "LNG" {
-  name                = "test-poc-hub-lng-01"
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
-  location            = azurerm_resource_group.hub-net-rg.location
+  name                = var.local-gateway-name
+  resource_group_name = azurerm_resource_group.hub-resource_rg.name
+  location            = var.location
   gateway_address     = azurerm_public_ip.on-prem-pip.ip_address
-  address_space       = ["10.0.0.0/24"]
+  address_space       = var.on-prem-address_prefixes-1
 }
 
 resource "azurerm_virtual_network_gateway_connection" "onpremise" {
-  name                = "connection"
-  location            = azurerm_resource_group.hub-net-rg.location
-  resource_group_name = azurerm_resource_group.hub-net-rg.name
+  name                = var.connection_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.hub-resource_rg.name
 
   type                       = "IPsec"
   virtual_network_gateway_id = azurerm_virtual_network_gateway.VNG.id
--- variable.tf
+++ variable.tf
@@ -20,32 +20,116 @@ variable "location" {
   description = "LOcation of on prem rg"
 }
 
-variable "Vnet-name" {
+variable "on-prem-vnet-name" {
   type = string
-  default = "test-VNet"
+  default = "test-poc-on-prem-vnet-01"
   description = "Name of Vnet"
 }
 
-variable "address_space" {
+variable "on-prem-address_space-1" {
+  type = set(string)
+  default = ["10.0.0.0/16"]
+  description = "address space of virtual network"
+}
+
+variable "on-prem-subnet-name" {
+  type = string
+  default = "test-poc-onprem-wrkld-subnet-01"
+  description = "name of subnet"
+}
+
+variable "on-prem-address_prefixes-1" {
+  type = list(string)
+  default = [ "10.0.0.0/24" ]
+  description = "Value of address prefixes"
+}
+
+variable "on-prem-public-ip" {
+  type = string
+  default = "test-poc-onprem-pip-01"
+  description = "on-prem-public-ip"
+}
+
+variable "on-prem-nsg" {
+  type = string
+  default = "test-poc-onprem-comp-nsg-01"
+  description = "Name of the on-prem nsg"
+}
+
+variable "on-prem-nic" {
+  type = string
+  default = "test-poc-onprem-nic-1"
+}
+
+variable "on-prem-hyper-v" {
+  type = string
+  default = "test-onprem-vm"
+}
+
+variable "hub_rg" {
+  type = string
+  default = "test-poc-hub-rg"
+  description = "The name of the resource group"
+}
+
+variable "hub-vnet-name" {
+  type = string
+  default = "test-poc-hub-vnet-01"
+  description = "Name of Vnet"
+}
+
+variable "hub_address_space" {
   type = set(string)
   default = ["10.1.0.0/16"]
   description = "address space of virtual network"
 }
 
-variable "Subnet-name" {
+variable "hub-subnet-name-1" {
   type = string
-  default = "AzureFirewallSubnet"
+  default = "GatewaySubnet"
   description = "name of subnet"
 }
 
-variable "address_prefixes" {
+variable "hub_address_prefixes-1" {
   type = list(string)
   default = [ "10.1.0.0/26" ]
   description = "Value of address prefixes"
 }
 
-variable "public-ip" {
+variable "hub-subnet-name-2" {
   type = string
-  default = "FW-pip"
-  description = "Firewall public IP"
+  default = "test-poc-hub-wrkld-subnet-01"
+  description = "name of subnet"
 }
+
+variable "hub_address_prefixes-2" {
+  type = list(string)
+  default = [ "10.1.0.64/26" ]
+  description = "Value of address prefixes"
+}
+
+variable "vng-public-ip" {
+  type = string
+  default = "test-poc-vng-pip-01"
+  description = "VNG public ip"
+}
+
+variable "VNG-name" {
+  type = string
+  default = "test-poc-vng"
+  description = "Name of the VNG"
+}
+
+variable "local-gateway-name" {
+  type = string
+  default = "test-poc-hub-lng-01"
+  description = "name of the lng"
+}
+
+variable "connection_name" {
+  type = string
+  default = "connection"
+  description = "name of the connection"
+}
+
+
